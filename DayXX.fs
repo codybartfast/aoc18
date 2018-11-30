4e7c2278caa4a06bb677b6e693bfc14eb6d54546
John Donnellan <codybartfast@gmail.com>
2018-11-30 08:28:08 +0000

    day01

---
+++ DayXX.fs
@@ -0,0 +1,31 @@
+﻿module DayXX
+
+#nowarn "0025"
+
+open System
+open System.Text.RegularExpressions
+open System.Collections.Generic
+
+let print obj = (printfn "%O" obj); obj
+let len (seq : seq<'a>) = Seq.length seq
+let toChars (str : string) = str.ToCharArray()
+let fromChars (chrs : char[]) = String(chrs)
+let encode (str : string) = System.Text.Encoding.ASCII.GetBytes(str);
+let toHex = BitConverter.ToString >> (fun str -> str.Replace("-", String.Empty))
+let groupValue (m:Match) (i:int) = m.Groups.[i].Value
+let rxMatch pattern str = Regex.Match(str, pattern)
+let rxMatches pattern str = Regex.Matches(str, pattern)
+let rxSplit pattern str = Regex.Split(str, pattern)
+let (||~) pred1 pred2 = (fun a -> (pred1 a) || (pred2 a))
+let (&&~) pred1 pred2 = (fun a -> (pred1 a) && (pred2 a))
+let filterCount predicate = Seq.filter predicate >> Seq.length
+
+(* ================ Part A ================ *) 
+
+let PartA input = 
+    input
+    
+(* ================ Part B ================ *)
+
+let PartB resultA input = 
+    "resultB"
---
+++ Program.fs
@@ -0,0 +1,40 @@
+﻿open System
+open System.IO
+open System.Diagnostics
+
+printfn ""
+let sw = Stopwatch ()
+let showTime () = 
+    printfn "(%s seconds)" (sw.Elapsed.TotalSeconds.ToString("n2"))
+    printfn ""
+
+let getInputFile name =
+    Path.Combine(__SOURCE_DIRECTORY__, "inputs", name) + ".txt"
+let readLines name = File.ReadAllLines(getInputFile name)
+let readText name = File.ReadAllText(getInputFile name)
+
+open Day01
+let name = "day01"
+
+let text = readText name
+let lines = readLines name
+
+[<EntryPoint>]
+let main argv =
+
+    let inputA = lines
+    
+    sw.Start()
+    let resultA = PartA inputA
+    printfn "%O" resultA 
+    showTime ()
+
+    let inputB = inputA
+
+    sw.Restart() 
+    let resultB = PartB resultA inputB
+    printfn "%O" resultB
+    showTime ()
+
+    Console.ReadKey() |> ignore
+    0
