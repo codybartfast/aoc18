bd733e751f9b8cc040b7c924270c1809f10cded1
John Donnellan <codybartfast@gmail.com>
2018-12-03 05:23:51 +0000

    day03 Part 1

--- Day03.fs
+++ Day03.fs
@@ -26,10 +26,31 @@ let print obj = (printfn "%O" obj); obj
 
 (* ================ Part A ================ *) 
 
+let parseLine line =
+    line
+    |> rxMatch "#(\d+) @ (\d+),(\d+): (\d+)x(\d+)"
+    |> (fun m -> 
+        let grp (i:int) =  m.Groups.[i].Value |> int
+        (grp 1), (grp 2, grp 3), ((grp 4), (grp 5)))
+
+
+let enumCoords (id, (l, t), (w, h)) =
+    seq{for x in l .. (l+(w-1)) do 
+        for y in t .. (t+(h-1)) do
+        yield (x,y)
+    }
+
 let Part1 input = 
     input |> toLines
+    |> List.map parseLine
+    |> List.map enumCoords
+    |> Seq.collect id
+    |> Seq.countBy id
+    |> filterCount (fun (_, count) -> count > 1)
+    
     
 
+      
 (* ================ Part B ================ *)
 
 let Part2 result1 input = 
