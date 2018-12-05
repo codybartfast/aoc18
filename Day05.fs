822e78b2eb6f1a367525d82903fe980fad293039
John Donnellan <codybartfast@gmail.com>
2018-12-05 08:36:33 +0000

    day05 Refactor

--- Day05.fs
+++ Day05.fs
@@ -1,67 +1,35 @@
-﻿(* a.cbf.pub/tx/___________________________________________/data.html *)
+﻿(* a.cbf.pub/tx/UALiYY7YM8d-TnYKH9gJoj3pfhUo8OdwhiUvG9G6WME/data.html *)
 
 module Day05
 
-// #nowarn "0025"
-
-open System
-open System.Text.RegularExpressions
-open System.Collections.Generic
-open System.Numerics
-open Microsoft.VisualBasic.CompilerServices
-
-let toLines (text:string) = text.Split('\n') |> List.ofSeq 
-let rec repeat item = seq{ yield item; yield! repeat item }
-let len (seq : seq<'a>) = Seq.length seq
-let toChars (str : string) = str.ToCharArray()
-let toString (chrs : seq<char>) = String(Array.ofSeq chrs)
-let encode (str : string) = System.Text.Encoding.ASCII.GetBytes(str);
-let toHex = BitConverter.ToString >> (fun str -> str.Replace("-", String.Empty))
-let groupValue (m:Match) (i:int) = m.Groups.[i].Value
-let rxMatch pattern str = Regex.Match(str, pattern)
-let rxMatches pattern str = Regex.Matches(str, pattern)
-let rxSplit pattern str = Regex.Split(str, pattern)
-let (||~) pred1 pred2 = (fun a -> (pred1 a) || (pred2 a))
-let (&&~) pred1 pred2 = (fun a -> (pred1 a) && (pred2 a))
-let filterCount predicate = Seq.filter predicate >> Seq.length
-let print obj = (printfn "%O" obj); obj
-
 (* ================ Part A ================ *) 
 
-let rec mark i (chars:char[]) =
-    let asciiVal indx = chars.[indx] |> int
-    match i with 
-    | i when i >= (chars.Length - 2) -> chars
-    | _ ->
-        if Math.Abs(asciiVal i - asciiVal (i + 1)) = 32 
-        then chars.[i] <- '_'; chars.[i + 1] <- '_'; mark (i + 1) chars
-        else  mark (i + 1) chars
+let toPolymer (input : string) = 
+    input
+    |> Seq.map int 
+    |> (List.ofSeq)
 
-let rec collapse (polymer:string) =
-    polymer
-    |> toChars
-    |> mark 0
-    |> toString
-    |> (fun str -> str.Replace("_", ""))
-    |> (fun newPoly ->
-        if len newPoly = len polymer 
-        then len newPoly
-        else collapse newPoly)
+let collapsedLength polymer =
+    let rec selectiveCopy source target = 
+        match source, target with
+            | [], target -> target
+            | sHead::sTail, [] -> selectiveCopy sTail [sHead]
+            | sHead::sTail, tHead::tTail ->  
+                if sHead - tHead |> abs  = 32 
+                    then selectiveCopy sTail tTail
+                    else selectiveCopy sTail (sHead::target)
+    selectiveCopy polymer []
+    |> List.length   
 
-    
-let Part1 input = 
-    input |> collapse
-
+let Part1 = toPolymer >> collapsedLength 
 
 (* ================ Part B ================ *)
 
-let removeType (polymer:string) (ut:int) =
-    polymer.Replace(ut |> char |> string, "")
-            .Replace(ut + 32 |> char |> string, "")
+let removeType polymer unitType =
+    polymer
+    |> List.filter (fun i -> i <> unitType && i <> (unitType + 32))
 
 let Part2 result1 input = 
-    [65..90]
-    |> Seq.map (fun ut -> removeType input ut)
-    |> Seq.map collapse
-    |> Seq.min
-
+    [(int 'A')..(int 'Z')]
+    |> List.map ((removeType (toPolymer input)) >> collapsedLength)
+    |> List.min
