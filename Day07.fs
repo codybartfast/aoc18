0e3c0a5d84d97088182f2abace414bcff1c3769f
John Donnellan <codybartfast@gmail.com>
2018-12-07 10:58:07 +0000

    day07 Part 2

--- Day07.fs
+++ Day07.fs
@@ -7,6 +7,7 @@ module Day07
 open System
 open System.Text.RegularExpressions
 open System.Collections.Generic
+open System.Drawing
 
 let toLines (text:string) = text.Split('\n') |> List.ofSeq 
 let rec repeat item = seq{ yield item; yield! repeat item }
@@ -46,7 +47,6 @@ let getDeps (data : (string*string) list) all =
     |> Seq.map (fun inst -> inst, (data |> Seq.filter (fun (b,f) -> f = inst) |> Seq.map fst |> Set))
     |> Map
     
-
 let rec findPath (deps : Map<string,Set<string>>) (soFar : string list)  (toDo : string Set) (available : string Set) =
     if available.IsEmpty then List.rev soFar else
     let instr = available |> Seq.sort |> Seq.head
@@ -57,10 +57,8 @@ let rec findPath (deps : Map<string,Set<string>>) (soFar : string list)  (toDo :
         |> Seq.filter (fun t -> Set.difference deps.[t] (Set soFar') = Set.empty) 
         |> Set
     findPath deps soFar' toDo' available'
-
-
-    
-let Part1 (input : string) =  //  "result1" (*
+  
+let Part1 (input : string) =    "result1" (*
     let data =
         input |> toLines |> List.map parseLine
     let (start, finish, all) = getEnds data
@@ -75,8 +73,59 @@ let Part1 (input : string) =  //  "result1" (*
 
 (* ================ Part B ================ *)
 
-let Part2 result1 (input : string) =   "result2" (*
-    input |> toLines
+let pickTask (deps : Map<string,Set<string>>) soFar toDo = 
+    let instrs = 
+        toDo
+        |> Seq.filter (fun t -> Set.difference deps.[t] (Set soFar) = Set.empty) 
+        |> Seq.sort |> List.ofSeq
+    match instrs with
+    | [] -> None
+    | instr::tail -> 
+        Some (instr, instr |> Seq.head |> int |> (fun a -> a - 4)) // <<<<<<--------  64 vs 4
+
+
+let rec startTasks deps soFar toDo (schedule : Map<int,string list>) elves time =
+    if elves = 0 then (toDo, schedule, elves) else
+
+    match pickTask deps soFar toDo with
+    | None -> (toDo, schedule, elves)
+    | Some (instr, duration) ->
+        let finish = time + duration
+        let completing = 
+            match schedule.ContainsKey finish with
+            | false -> [instr]
+            | true -> instr::(schedule.[finish])
+        startTasks 
+            deps
+            soFar 
+            (Set.remove instr toDo) 
+            (schedule.Add(finish, completing))
+            (elves - 1)
+            time 
+
+let rec assign (deps : Map<string,Set<string>>) (soFar : string list) (toDo : string Set) (schedule : Map<int,string list>) (elves : int)  time =
+    
+    match toDo.Count, schedule.Count with
+    | 0, 0 -> time, soFar
+    | _ ->
+    
+
+    let (time, completed) = schedule |> Map.toSeq |> Seq.minBy fst
+    let currentSchedule = schedule.Remove time
+    let soFar' = (completed |> List.sort) @ soFar
+    let (toDo', schedule', elves') = startTasks deps soFar' toDo currentSchedule (elves + 1) time
+    assign deps soFar' toDo' schedule' elves' time
+  
+
+let Part2 result1 (input : string) =  // "result2" (*
+    let data =
+        input |> toLines |> List.map parseLine
+    let (start, finish, all) = getEnds data
+
+    let (time, path) = assign (getDeps data all) [] all  (Map [(0, [] )]) 5 0 // <<<<<<-------- 2 vs 5
+    (time, path |> List.rev |> String.concat "")
+
+
 
 
 
