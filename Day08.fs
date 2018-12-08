8b8a3244e76096feffe0a83f39a2ccf527a9472f
John Donnellan <codybartfast@gmail.com>
2018-12-08 07:45:35 +0000

    day08 Refactor

--- Day08.fs
+++ Day08.fs
@@ -1,55 +1,33 @@
-﻿(* a.cbf.pub/tx/___________________________________________/data.html *)
+﻿(* a.cbf.pub/tx/O4E39PY71rUtmkOLkXFBvFhYOoH9f64V_NzUCK3ebrM/data.html *)
 
 module Day08
 
-// #nowarn "0025"
-
-open System
-open System.Text.RegularExpressions
-open System.Collections.Generic
-
-let toLines (text:string) = text.Split('\n') |> List.ofSeq 
-let rec repeat item = seq{ yield item; yield! repeat item }
-let len (seq : seq<'a>) = Seq.length seq
-let toChars (str : string) = str.ToCharArray()
-let toString (chrs : seq<char>) = String(Array.ofSeq chrs)
-let encode (str : string) = System.Text.Encoding.ASCII.GetBytes(str);
-let toHex = BitConverter.ToString >> (fun str -> str.Replace("-", String.Empty))
-let groupValue (m:Match) (i:int) = m.Groups.[i].Value
-let rxMatch pattern str = Regex.Match(str, pattern)
-let rxMatches pattern str = Regex.Matches(str, pattern)
-let rxSplit pattern str = Regex.Split(str, pattern)
-let (||~) pred1 pred2 = (fun a -> (pred1 a) || (pred2 a))
-let (&&~) pred1 pred2 = (fun a -> (pred1 a) && (pred2 a))
-let filterCount predicate = Seq.filter predicate >> Seq.length
-let print obj = (printfn "%O" obj); obj
+#nowarn "0025"
 
 (* ================ Part A ================ *) 
 
-let readMetadata (data, mdTotal) mdCount =
-    (List.skip mdCount data)
-    , mdTotal + (List.take mdCount data |> List.sum)
+let parse (input:string) = 
+    input.Split(' ') |> List.ofArray |> List.map (int)
+
+let readMetadata (data, mdSum) mdCount =
+    (data |> List.skip mdCount)
+    , mdSum + List.sum (data |> List.take mdCount)
 
 let rec readChild (data, mdTotal) =
     match data with
-    | [] -> ([], mdTotal)
-    | [_] -> failwith "oops"
     | chCount::(mdCount::tail) -> 
         let (data, mdTotal) =
             ((tail, mdTotal), [1..chCount])
             ||> List.fold (fun state _ -> readChild state)
         readMetadata (data, mdTotal) mdCount
 
-let Part1 (input : string) =  //  "result1" (*
-    let data = input.Split(' ') |> List.ofArray |> List.map (int)
-    readChild (data, 0)
-//*)
-
+let Part1 (input : string) =
+    readChild (parse input, 0) |> snd
     
 
 (* ================ Part B ================ *)
 
-let calcValue data mdCount childValues =
+let readMetadataValue data mdCount childValues =
     let metadata = List.take mdCount data
     let childCount = Array.length childValues
     let value = 
@@ -57,28 +35,17 @@ let calcValue data mdCount childValues =
              List.sum metadata
          else 
             metadata
-            |> List.filter (fun idx -> idx <= childCount)
-            |> List.map (fun idx -> childValues.[idx  - 1])
-            |> List.sum
-    (List.skip mdCount data), value
+            |> List.filter ((>=) childCount)
+            |> List.sumBy (fun idx -> childValues.[idx  - 1])
+    value, (List.skip mdCount data)
 
-let rec readValue data : (int list * int) =
+let rec readValue data  =
     match data with
-    | [] -> ([], -1)
-    | [_] -> failwith "oops"
     | chCount::(mdCount::tail) -> 
-        let (valuesList, data) =
+        let (values, data) =
             (tail, [1..chCount])
-            ||> List.mapFold (fun data _ -> 
-                let data, value = (readValue data)
-                (value, data))
-        let values = Array.ofList valuesList
-        calcValue data mdCount values
+            ||> List.mapFold (fun data _ -> (readValue data))
+        readMetadataValue data mdCount (values |> Array.ofList)
 
-
-let Part2 result1 (input : string) = //  "result2" (*
-    let data = input.Split(' ') |> List.ofArray |> List.map (int)
-    readValue data
-
-
-//*)
+let Part2 result1 (input : string) =
+    readValue (parse input) |> fst
