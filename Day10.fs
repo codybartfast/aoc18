508fe0f2839e0ca5120f4f9e29aff22bc60a6d9f
John Donnellan <codybartfast@gmail.com>
2018-12-10 06:21:51 +0000

    day10 Part 1

--- Day10.fs
+++ Day10.fs
@@ -26,15 +26,61 @@ let print obj = (printfn "%O" obj); obj
 
 (* ================ Part A ================ *) 
 
-let parseLine  = rxMatch "(\d+)\D+(\d+)" >> fun mtch ->
+//position=<-42281, -42228> velocity=< 4,  4>
+let parseLine  = rxMatch "\D+?(-?\d+)\D+?(-?\d+)\D+?(-?\d+)\D+?(-?\d+)" >> fun mtch ->
     let grp idx = groupValue mtch idx
     let grpi = grp >> int
-    grp 1, grp 2
+    (grpi 1, grpi 2), (grpi 3, grpi 4) 
+
+let displayLine points : unit =
+    let set = points |> List.map fst |> Set
+    [0..99]
+    |> Seq.iter(fun i -> 
+        if set.Contains i then Console.Write("#")
+        else Console.Write(" "))
+    Console.WriteLine()
+        
+let display (lights : (int * int) list) =
+    let xMin =
+        lights 
+        |> List.groupBy fst |> List.minBy fst |> fst
+    let yMin =
+        lights 
+        |> List.groupBy snd |> List.minBy fst |> fst
+    let relCoords =
+        lights |> List.map (fun (x,y) -> (x - xMin, y - yMin))
+    let visible =
+        relCoords |> List.filter (fun (x, y) -> (x < 100) && (y < 100))
+    let rows = 
+        visible |> List.groupBy snd |> Map
+    [0..30]
+    |> List.iter (fun i ->
+        if rows.ContainsKey i then displayLine (rows.[i])
+        else Console.WriteLine())
+ 
+let lightSequence (lights : (int * int) list) (vectors : (int * int) list) =
+    (lights, Seq.initInfinite id)
+    ||> Seq.scan (fun lights _ -> 
+        (lights, vectors) ||>  List.map2 (fun (lx, ly) (vx, vy) -> (lx + vx), (ly + vy)))
+ 
+let spread lights =
+    let xs = lights |> List.map fst |> List.sort
+    let ys = lights |> List.map snd |> List.sort
+    (List.last xs - List.head xs) + (List.last ys - List.head ys)
     
 let Part1 (input : string) =  // "result1" (*
-    input |> toLines |> Seq.map parseLine
-
-
+    let data = input |> toLines |> List.map parseLine
+    let lights = data |> List.map fst
+    let vectors = data |> List.map snd
+    let sequence = lightSequence lights vectors
+    
+    sequence
+    |> Seq.skipWhile (fun lights -> (spread lights) > 300)
+    |> Seq.iter(fun lights ->
+        Console.WriteLine(String('_', 50))
+        display lights
+        Console.ReadKey() |> ignore)
+    
 
 //*)
 
