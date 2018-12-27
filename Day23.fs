5794559ad41f2483022cd0c94d54b1c0861a324d
John Donnellan <codybartfast@gmail.com>
2018-12-27 18:56:12 +0000

    corner vectors

--- Day23.fs
+++ Day23.fs
@@ -61,83 +61,40 @@ let Part1 (input : string) =
 
 (* ================ Part B ================ *)
 
+type Corner = A|N|S|W|E|B
+
 let rangeCount bots loc =
     bots 
     |> Seq.filter (fun (cntr, rad) -> rad >= distance loc cntr)
     |> Seq.length
-
-let leftSlope ((x,y),r) = (x-y)-r
-let rightSlope ((x,y), r) = (x-y)+r
-let leftBackslope ((x,y), r) = (x+y)-r
-let rightBackslope ((x,y), r) = (x+y)+r
-
-//let slopeIntersects dmnd1 dmnd2 =
-//    let left1 = leftSlope dmnd1
-//    let right1 = rightSlope dmnd1
-//    let left2 = leftSlope dmnd2
-//    let right2 = rightSlope dmnd2
-//    let xxx = (left2 <= left1 && left1 <= right2)
-//                || (left1 <= left2 && left2 <= right1)
-//    xxx
-    
-//let backslopeIntersects dmnd1 dmnd2 =
-//    let left1 = leftBackslope dmnd1
-//    let right1 = rightBackslope dmnd1
-//    let left2 = leftBackslope dmnd2
-//    let right2 = rightBackslope dmnd2
-//    let xxx = (left2 <= left1 && left1 <= right2)
-//                || (left1 <= left2 && left2 <= right1)
-//    xxx
-
-//let intersects dmnd1 dmnd2 = 
-//    slopeIntersects dmnd1 dmnd2
-//    && backslopeIntersects dmnd1 dmnd2
 
 let doIntersect (cntr1,r1) (cntr2,r2) =
     let radtotal = r1 + r2
     let distance = distance cntr1 cntr2
     distance <= radtotal
 
-let getCorners dmnd1 dmnd2 =
-    let corners slope otherNear otherFar fct =
-        let distN = otherNear - slope
-        let countN = distN - (distN / 2L)
-        let c1 = (slope + countN), (fct * countN)
-        let distF = otherFar - slope     
-        let countF = distF / 2L
-        let c2 = (slope + countF), (fct * countF)
-        [c1; c2]
-    let leftSlope = leftSlope dmnd1
-    let rightSlope = rightSlope dmnd1
-    let leftBackslope = leftBackslope dmnd2
-    let rightBackslope = rightBackslope dmnd2
-    Seq.collect id [
-        corners leftSlope leftBackslope rightBackslope 1L
-        corners rightSlope leftBackslope rightBackslope 1L
-        corners rightBackslope rightSlope leftSlope -1L
-        corners leftBackslope rightSlope leftSlope -1L]
+let getCorners ((x,y,z), r) =
+    [(  (x,y,z+r), A);
+            ((x,y+r,z), N);
+        ((x-r,y,z), W); ((x+r,y,z), E);
+            ((x,y-r,z), S);
+        ((x,y,z-r), B);]
 
+let getInsideCornders bot1 bot2 =
+    let (c1, r1) = bot1
+    getCorners bot2
+    |> List.filter(fun (loc,crn) -> distance c1 loc <= r1)
+    
+
+let getPoi (bot1, bot2) =
+    if bot1 = bot2 then [] else
+    let (c1, r1), (c2, r2) = bot1, bot2
+    if r2 > r1 then [] else
+    if (not (doIntersect bot1 bot2)) then [] else
+    let xxx = getInsideCornders bot1 bot2
+    [1]
 
 let Part2 result1 (input : string) = // "result2" 
     let bots = input |> toLines |> List.map parseLine
     seq{ for bot1 in bots do for bot2 in bots do yield (bot1, bot2)}
-    |> Seq.collect(fun (bot1,bot2) -> 
-        let intersect = doIntersect bot1 bot2
-        if (not intersect) then Seq.empty else
-        let (x1, y1, z1), r1 = bot1
-        let (x2, y2, z2), r2 = bot2
-        Seq.collect id [
-            getCorners ((x1, y1), r1) ((x2, y2), r2) 
-                |> Seq.collect (fun (x,y) -> [(x,y,z1); (x,y,z2)])
-            getCorners ((y1, z1), r1) ((y2, z2), r2)
-                |> Seq.collect (fun (y,z) -> [(x1,y,z); (x2,y,z)])
-            getCorners ((x1, z1), r1) ((x2, z2), r2)
-                    |> Seq.collect (fun (x,z) -> [(x,y1,z); (x,y2,z)])])
-        //|> Seq.map (rangeCount bots)
-        //|> Seq.max
-
-        |> Seq.map (fun loc -> loc, rangeCount bots loc)
-        |> Seq.filter (fun (loc, count) -> count = 898)
-        |> Seq.map (fun (loc, count) -> loc, distance loc (0L,0L,0L))
-        |> Seq.minBy snd
-
+    |> Seq.map getPoi
